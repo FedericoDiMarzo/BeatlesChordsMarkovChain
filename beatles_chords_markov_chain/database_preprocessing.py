73bec10914f54e972e8f7ec9f6cfa5956da858b5
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-05-07 17:04:24 +0200

    Update database_preprocessing.py
    handling root notes

--- beatles_chords_markov_chain/database_preprocessing.py
+++ beatles_chords_markov_chain/database_preprocessing.py
@@ -20,9 +20,9 @@ def get_chord_roots():
     chords_sequence_dict = {}
 
     # iterating inside the chord_path folder
-    for filename in os.listdir(paths.chords_path):
+    for filename in os.listdir(chords_path):
         # reading the file
-        new_dataframe = pd.read_csv(os.path.join(paths.chords_path, filename), sep=' ', header=None)
+        new_dataframe = pd.read_csv(os.path.join(chords_path, filename), sep=' ', header=None)
 
         # removing the rows with N as a chord (because the rests are not needed)
         new_dataframe = new_dataframe.drop(new_dataframe[new_dataframe[2] == 'N'].index)
@@ -43,17 +43,20 @@ def get_chord_roots():
     return chords_sequence_dict
 
 
-def get_chord_numbers(chord_roots):
+def convert_notes_to_numbers(notes_dict):
     """
     Maps each chord root inside the sequences into values between 0 and 11.
 
-    :param chord_roots: dictionary of chord roots lists
+    :param notes_dict: dictionary of chord roots lists
     :return: dictionary of numeric notes list
     """
     chord_numbers = {}
-    for song, chords in chord_roots_dict.items():
-        # creates a new list with the new mapping
-        chord_numbers[song] = [amp.musical_note_to_midi(x) for x in chords]
+    for song, chords in notes_dict.items():
+        if isinstance(chords, list):
+            # creates a new list with the new mapping
+            chord_numbers[song] = [amp.musical_note_to_midi(x) for x in chords]
+        else:
+            chord_numbers[song] = amp.musical_note_to_midi(chords)
     return chord_numbers
 
 
@@ -63,13 +66,23 @@ def get_rootkeys():
     rootkey_dict = {}
 
     # iterating inside the chord_path folder
-    for filename in os.listdir(paths.chords_path):
+    for filename in os.listdir(rootkey_path):
         # reading the file
-        new_dataframe = pd.read_csv(os.path.join(paths.chords_path, filename), sep=' ', header=None)
+        new_dataframe = pd.read_csv(os.path.join(rootkey_path, filename), sep='\t', header=None)
+        if new_dataframe.shape[0] == 1:
+            key = list(new_dataframe[3])[0]
+            if ':' not in key:
+                rootkey_dict[filename] = key
+    return rootkey_dict
 
 
 # one list of chord roots for each song
 chord_roots_dict = get_chord_roots()
 
 # one list of numeric note values for each song
-chord_numbers_dict = get_chord_numbers(chord_roots_dict)
+chord_numbers_dict = convert_notes_to_numbers(chord_roots_dict)
+
+
+rootkey_dict = get_rootkeys()
+
+rootkey_numbers_dict = convert_notes_to_numbers(rootkey_dict)
