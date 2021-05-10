5e923bc716183d6a757ee4876672f70d5d6d1f60
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-05-10 15:50:10 +0200

    Update processed_data.py
    comments

--- beatles_chords_markov_chain/processed_data.py
+++ beatles_chords_markov_chain/processed_data.py
@@ -13,7 +13,9 @@ rootkey_dict = get_rootkeys()
 # one rootkey as a numeric value (between 0 and 11) for each song
 rootkey_numbers_dict = convert_notes_to_numbers(rootkey_dict)
 
+# one list of intervals for each song
 intervals_dict = {}
 
+# filling intervals_dict
 for song, rootkey_number in rootkey_numbers_dict.items():
     intervals_dict[song] = get_intervals(chord_numbers_dict[song], rootkey_number)
