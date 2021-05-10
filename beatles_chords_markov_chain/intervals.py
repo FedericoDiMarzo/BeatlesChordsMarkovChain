c1eddd79009d556249a6491d2d8c9dc9a47b031c
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-05-10 15:51:17 +0200

    Update intervals.py
    comments

--- beatles_chords_markov_chain/intervals.py
+++ beatles_chords_markov_chain/intervals.py
@@ -1,3 +1,4 @@
+# symbols used to represent chord intervals
 interval_symbols = {
     0: 'I',
     2: 'II',
@@ -10,7 +11,15 @@ interval_symbols = {
 
 
 def get_intervals(chords_numbers, root_number):
-    # TODO: masking
+    """
+    Given a certain chord number list and the keyroot number, it returns
+    the list of intervals for that progression. The progression is supposed
+    to be in Ionian mode.
+
+    :param chords_numbers: list of midi value (between 0 and 11) of the root of the chord
+    :param root_number: midi value (between 0 and 11) of the keyroot
+    :return: list of intervals
+    """
     intervals = [x - root_number for x in chords_numbers]
     intervals = [x + 11 if x < 0 else x for x in intervals]
     intervals = [interval_symbols[x] for x in intervals if x in interval_symbols.keys()]
