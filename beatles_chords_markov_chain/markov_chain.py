cd7abb5f2ce0d6e2bbd8ba0fea687e64ce9c0a94
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-05-10 15:50:22 +0200

    Update markov_chain.py
    training the markov chain

--- beatles_chords_markov_chain/markov_chain.py
+++ beatles_chords_markov_chain/markov_chain.py
@@ -1,10 +1,21 @@
 import pomegranate as pg
 from processed_data import intervals_dict
 
+# order of the markov chain
+markov_chain_order = 3
+
 
 def train_markov_chain(intervals_sample_dict):
+    """
+    given the dictionary containing the lists of intervals for each song,
+    it returns the markov chain object trained with the data.
+
+    :param intervals_sample_dict: dictionary having the song names as keys and lists of intervals as values
+    :return: trained markov chain object
+    """
     samples = intervals_sample_dict.values()
-    return pg.MarkovChain.from_samples(samples)
+    return pg.MarkovChain.from_samples(samples, k=markov_chain_order)
 
 
+# trained markov chain object
 chords_markov_chain = train_markov_chain(intervals_dict)
